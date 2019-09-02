eef495e84161fdeb11dad6544281cdb959a43fbb
joaoc <joao.carabetta@gmail.com>
2019-09-02 17:48:25 +0100

    add test install.sh

---
+++ project-templates/install.sh
@@ -0,0 +1,4 @@
+mkdir ~/.project-templates
+
+curl https://codeload.github.com/JoaoCarabetta/project-templates/tar.gz/issue-22 | tar -xz --strip=2 project-templates-issue-22/project-templates -C ~/.project-templates
+
