531d258cda36255a10b048ba25282c345a29776a
joaoc <joao.carabetta@gmail.com>
2019-09-02 18:28:31 +0100

    One more install.sh test

--- project-templates/install.sh
+++ project-templates/install.sh
@@ -1,4 +1,18 @@
+# Add config files to ~/.project-templates
 mkdir ~/.project-templates
 
-curl https://codeload.github.com/JoaoCarabetta/project-templates/tar.gz/issue-22 | tar -xz --strip=2 project-templates-issue-22/project-templates -C ~/.project-templates
+cd ~/.project-templates
 
+# TODO: change url to master
+curl https://codeload.github.com/JoaoCarabetta/project-templates/tar.gz/issue-22 | tar -xz --strip=2 project-templates-issue-22/project-templates 
+
+# Add executables.sh to `~/.bash_profile
+
+if ! grep -Fxq "source ~/.project-templates/executables.sh" ~/.bash_profile 
+then
+    echo "source ~/.project-templates/executables.sh" >> ~/.bash_profile
+fi
+
+# Restart Terminal
+
+echo "Please restart your terminal"
