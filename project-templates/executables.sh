583876f44689c175075d94af3d965a75e6ec6c57
Fernanda Scovino <fscovinom@gmail.com>
2019-09-04 17:29:59 -0300

    lets debug

--- project-templates/executables.sh
+++ project-templates/executables.sh
@@ -18,43 +18,45 @@ function start_project() {
        read -r name <<<$(grep name ~/.project-templates/config.yaml)
        read -r username <<<$(grep username ~/.project-templates/config.yaml)
        read -r email <<<$(grep email ~/.project-templates/config.yaml)
-   else [ $credentials = 'N']
-       printf "\n>>> Enter your GitHub username: www.github.com/<USERNAME>/\n"
-       read username
-       printf "\n>>> And also your name with \" - like \"Fernanda Scovino\":\n"
-       read name
-       printf "\n>>> Please, enter your GitHub email:\n"
-       read email
-   fi
+       printf $name
 
-   curl -u $username https://api.github.com/user/repos -d {\"name\":\"$project\"}
+#    else [ $credentials = 'N']
+#        printf "\n>>> Enter your GitHub username: www.github.com/<USERNAME>/\n"
+#        read username
+#        printf "\n>>> And also your name with \" - like \"Fernanda Scovino\":\n"
+#        read name
+#        printf "\n>>> Please, enter your GitHub email:\n"
+#        read email
+#    fi
 
-   printf "\n>>>> Great! Creating local repository...\n"
-   mkdir $project
-   cd $project
+#    curl -u $username https://api.github.com/user/repos -d {\"name\":\"$project\"}
 
-   printf "\n>>> Downloading basic folder...\n";
-   curl https://codeload.github.com/JoaoCarabetta/project-templates/tar.gz/master | tar -xz --strip=2 project-templates-master/basic
+#    printf "\n>>>> Great! Creating local repository...\n"
+#    mkdir $project
+#    cd $project
 
-   printf "\n>>> Inicializing git and set remote...\n";
-   git init
-   git remote add origin http://github.com/$username/$project.git
+#    printf "\n>>> Downloading basic folder...\n";
+#    curl https://codeload.github.com/JoaoCarabetta/project-templates/tar.gz/master | tar -xz --strip=2 project-templates-master/basic
 
-   echo "\n>>> Thank you! Setting user's config...\n";
-   git config user.name $name
-   git config user.email \"$email\"
+#    printf "\n>>> Inicializing git and set remote...\n";
+#    git init
+#    git remote add origin http://github.com/$username/$project.git
 
-   printf "\n>>> Adding changes and initial commit...\n";
-   git add .
-   git commit -m "initial commit: folder structure"
+#    echo "\n>>> Thank you! Setting user's config...\n";
+#    git config user.name $name
+#    git config user.email \"$email\"
 
-   printf "\n>>> Uploading changes...\n";
-   git push --set-upstream origin master
+#    printf "\n>>> Adding changes and initial commit...\n";
+#    git add .
+#    git commit -m "initial commit: folder structure"
 
-   printf "\n>>> Setting up virtual environment\n"
-   python3 -m venv $project
-   source $project/bin/activate
-   pip install -r requirements.txt
+#    printf "\n>>> Uploading changes...\n";
+#    git push --set-upstream origin master
 
-   printf "\n>>> All done! :D (to exit venv, enter deactivate on terminal)\n";
-}
+#    printf "\n>>> Setting up virtual environment\n"
+#    python3 -m venv $project
+#    source $project/bin/activate
+#    pip install -r requirements.txt
+
+#    printf "\n>>> All done! :D (to exit venv, enter deactivate on terminal)\n";
+# }
